175cbc32abc439819df1f42f2b803197b5cfe762
Nwanochie Emanuel <53648442+wenotch@users.noreply.github.com>
2020-09-27 14:49:10 +0100

    Add files via upload

--- src/components/Burger/Buildcontrols/Buildcontrols.js
+++ src/components/Burger/Buildcontrols/Buildcontrols.js
@@ -1,8 +1,30 @@
 import React from "react";
-import classes form "./Buildcontrol.module.css"
-const buildcontrols =(props)=>(
-    <div>
+import classes from "./Buildcontrols.module.css";
+import BuilControl from "./Buildcontrol/Buildcontrol";
+const controls = [
+  { label: "Salad", type: "salad" },
+  { label: "Bacon", type: "bacon" },
+  { label: "Cheese", type: "cheese" },
+  { label: "Meat", type: "meat" },
+];
+const buildcontrols = (props) => (
+  <div className={classes.Buildcontrols}>
+    <p>
+      Current price is: <strong> {props.price.toFixed(2)}</strong>
+    </p>
+    {controls.map((ctrl) => (
+      <BuilControl
+        key={ctrl.label}
+        label={ctrl.label}
+        added={() => props.ingredientAdded(ctrl.type)}
+        removed={() => props.ingredientremove(ctrl.type)}
+        disabled={props.disabl[ctrl.type]}
+      />
+    ))}
 
-    </div>
-)
+    <button className={classes.OrderButton} disabled={!props.purchasable}>
+      ORDER NOW
+    </button>
+  </div>
+);
 export default buildcontrols;
